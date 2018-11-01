80d159a6c5de453e1ee3060b720b8e9825d2923f
Hendrik <hubeyciti@gmail.com>
2018-11-01 09:42:24 +0100

    Comments added

--- LinkedListLibrary/LinkedList.cs
+++ LinkedListLibrary/LinkedList.cs
@@ -4,59 +4,90 @@ namespace LinkedListLibrary
 {
     public class LinkedList
     {
+        /// <summary>
+        /// First element of the linked list
+        /// </summary>
         public Element First { get; set; }
 
+        /// <summary>
+        /// Constructs a linked list with empty Element
+        /// </summary>
         public LinkedList()
         {
             First = null;
         }
 
+        /// <summary>
+        /// <para>Adds an element to the linked list.</para>
+        /// <para>If an element already exists it takes the First element</para>
+        /// and attaches it to the Next property.
+        /// <para>If the list is empty, the element is just added to the list.</para>
+        /// </summary>
+        /// <param name="element">Used to add to the list</param>
         public void AddFirst(Element element)
         {
+            // Check if Element is empty
             if (First == null)
             {
+                // Sets the first element
                 First = element;
             } else
             {
+                // Sets the Next element
                 element.Next = First;
+                // Sets the First element
                 First = element;
             }
         }
 
+        /// <summary>
+        /// Removes the first element of the list
+        /// </summary>
         public void RemoveFirst()
         {
-            if(First == null)
+            // Check if Element is empty
+            if (First == null)
             {
                 Console.WriteLine("Ingen Data i linkedlist");
             } else
             {
-                Element next = First.Next;
-                First = null;
-                First = next;
+                // Sets the next Element as First Element
+                // Garbage collector will make sure to delete unused Element
+                First = First.Next;
             }
         }
 
+        /// <summary>
+        /// Counts the elements in the list
+        /// </summary>
+        /// <returns>Integer representing number of elements in the list</returns>
         public int Count()
         {
+            // Make a reference to the First element.
+            // Otherwise it will be destructive count
             Element localFirst = First;
             int counter = 0;
             bool end = false;
             while (!end)
             {
+                // If localFirst is empty, there are no elements in the list
                 if (localFirst == null)
                 {
                     end = true;
                 }
                 else
                 {
+                    // There are element(s) in the list, increment counter with 1
                     counter++;
+                    // See if there's an next element in the first element
                     if (localFirst.Next == null)
                     {
+                        // We are at the end of the list
                         end = true;
                     }
                     else
                     {
-
+                        // Set the next element as first element
                         localFirst = localFirst.Next;
                     }
                 }
@@ -64,27 +95,37 @@ namespace LinkedListLibrary
             return counter;
         }
 
+        /// <summary>
+        /// Returns the linked list as string.
+        /// </summary>
+        /// <returns>The list element.data as string</returns>
         public string Print()
         {
+            // Make a reference to the First element.
+            // Otherwise it will be destructive print
             Element localFirst = First;
             string result = "";
             bool end = false;
             while (!end)
             {
+                // If localFirst is empty, there are no elements in the list
                 if (localFirst == null)
                 {
+                    // We are at the end of the list
                     end = true;
                 }
                 else
                 {
+                    // There are element(s) in the list, add the data to the result
                     result += localFirst.Data.ToString() + "\n";
                     if (localFirst.Next == null)
                     {
+                        // We are at the end of the list
                         end = true;
                     }
                     else
                     {
-
+                        // Set the next element as first element
                         localFirst = localFirst.Next;
                     }
                 }
@@ -92,13 +133,18 @@ namespace LinkedListLibrary
             return result;
         }
 
+        /// <summary>
+        /// Sorts the list in the ascending order
+        /// </summary>
         public void Sort()
         {
-            LinkedList linkedList = new LinkedList();
+            // Make a reference to the First element.
+            
             int linkedCount = Count();
             
             for (int i = 0; i < linkedCount; i++)
             {
+                // Otherwise it will be destructive sort
                 Element localFirst = First;
                 bool end = false;
                 while (!end)
--- LinkedListTest/LinkedListTest.cs
+++ LinkedListTest/LinkedListTest.cs
@@ -9,18 +9,27 @@ namespace LinkedListTest
         [TestMethod]
         public void AddFirstTest()
         {
+            // Arrange
             LinkedList linkedList = new LinkedList();
-            Element element = new Element();
 
-            element.Data = 7;
+            Element element3 = new Element();
+            linkedList.AddFirst(element3);
+            Element element2 = new Element();
+            linkedList.AddFirst(element2);
+            Element element1 = new Element();
+            element1.Data = 7;
 
-            linkedList.AddFirst(element);
-            Assert.AreEqual<int>(element.Data, linkedList.First.Data);
+            // Act
+            linkedList.AddFirst(element1);
+
+            // Assert
+            Assert.AreEqual<int>(element1.Data, linkedList.First.Data);
         }
 
         [TestMethod]
         public void RemoveFirstTest()
         {
+            // Arrange
             LinkedList linkedList = new LinkedList();
             Element element2 = new Element();
             element2.Data = 10;
@@ -30,14 +39,18 @@ namespace LinkedListTest
             element1.Data = 7;
             linkedList.AddFirst(element1);
 
+            // Act
             linkedList.RemoveFirst();
 
-            Assert.AreEqual<int>(element2.Data, linkedList.First.Data);
+            // Assert
+            Element expected = element2;
+            Assert.AreEqual<int>(expected.Data, linkedList.First.Data);
         }
 
         [TestMethod]
         public void CountTest()
         {
+            // Arrange
             LinkedList linkedList = new LinkedList();
             Element element2 = new Element();
             element2.Data = 10;
@@ -47,12 +60,18 @@ namespace LinkedListTest
             element1.Data = 7;
             linkedList.AddFirst(element1);
 
-            Assert.AreEqual<int>(2, linkedList.Count());
+            // Act
+            int actual = linkedList.Count();
+
+            // Assert
+            int expected = 2;
+            Assert.AreEqual<int>(expected, actual);
         }
 
         [TestMethod]
         public void PrintTest()
         {
+            // Arrange
             LinkedList linkedList = new LinkedList();
             Element element2 = new Element();
             element2.Data = 10;
@@ -62,13 +81,18 @@ namespace LinkedListTest
             element1.Data = 7;
             linkedList.AddFirst(element1);
 
+            // Act
+            string actual = linkedList.Print();
+
+            // Assert
             string expected = "7\n10\n";
-            Assert.AreEqual(expected, linkedList.Print(), true);
+            Assert.AreEqual(expected, actual, true);
         }
 
         [TestMethod]
         public void SortTest()
         {
+            // Arrange
             LinkedList linkedList = new LinkedList();
             Element element4 = new Element();
             element4.Data = 7;
@@ -85,10 +109,14 @@ namespace LinkedListTest
             Element element1 = new Element();
             element1.Data = 10;
             linkedList.AddFirst(element1);
-            linkedList.Sort();
 
+            // Act
+            linkedList.Sort();
+            string actual = linkedList.Print();
+
+            // Assert
             string expected = "1\n2\n7\n10\n";
-            Assert.AreEqual(expected, linkedList.Print());
+            Assert.AreEqual(expected, actual);
 
         }
     }
